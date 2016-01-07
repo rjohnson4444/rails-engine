b40e286c252359595abad0a3ea385cb401a6f7f9
Ryan Johnson <rjohnson4444@live.com>
2016-01-07 02:01:42 -0700

    delete helpers - for testing purposes

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  
-
-  test "#favorite_merchant returns a merchant" do
-    customer = Customer.first
-
-    binding.pry
-    customer.favorite_merchant
-  end
+  # test "#favorite_merchant returns a merchant" do
+  #   customer = Customer.first
+  #
+  #   customer.favorite_merchant
+  # end
 end
